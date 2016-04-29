28b3473d3ebce4f6cad55f195de8bb4766f1f595
Roy Kollen Svendsen <roykollensvendsen@gmail.com>
2016-04-29 21:42:55 +0200

    Modify to run in older node.js (raspbian APT-package)

--- relay.js
+++ relay.js
@@ -3,9 +3,9 @@ var spawn = require("child_process").spawn;
 var Relay = (function () {
    var self = this; 
    self.child = spawn("python", ["-u", "./python/relay.py"]);
-   self.child.stdout.on("data",(data)=>{console.log("stdout: " + data);});
-   self.child.stderr.on("data",(data)=>{console.log("stderr: " + data);});
-   self.child.stderr.on("close",()=>{console.log("closed");});
+   self.child.stdout.on("data",function(data){console.log("stdout: " + data);});
+   self.child.stderr.on("data",function(data){console.log("stderr: " + data);});
+   self.child.stderr.on("close",function(){console.log("closed");});
    return {
       turnOn: function () {
          self.child.stdin.write("on\n");
